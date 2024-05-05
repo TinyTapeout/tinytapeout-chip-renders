7cb74184994e928c898ec0d73bd13fbafa2f9e26
Uri Shaked <uri@urishaked.com>
2024-05-05 11:05:37 +0300

    Initial commit: tt02 through tt06 chip renders

---
+++ README.md
@@ -0,0 +1,68 @@
+# Tiny Tapeout Chip Renders
+
+This repo contains the chip renders for all Tiny Tapeouts shuttles since TT02. Each shuttle includes two renders:
+
+- full_gds.png: A full render of the GDS file.
+- logic_density: Shows only the local interconnect layer, which is used to estimate the logic density of the chip.
+
+For Tiny Tapeout 4 and later, the renders include the complete chip area, including the pads, and the caravel harness. For earlier shuttles, the renders only include the core area.
+
+The renders are generated using the [render.py](scripts/render.py) script, which uses [klayout](https://www.klayout.org/) to render the GDS files. See [below](#regenerating-the-renders) for instructions on how to regenerate the renders.
+
+## Full Renders
+
+### Tiny Tapeout 6
+
+[![Tiny Tapeout 6](shuttles/tt06/full_gds.png)](shuttles/tt06/full_gds.png)
+
+### Tiny Tapeout 5
+
+[![Tiny Tapeout 5](shuttles/tt05/full_gds.png)](shuttles/tt05/full_gds.png)
+
+### Tiny Tapeout 4
+
+[![Tiny Tapeout 4](shuttles/tt04/full_gds.png)](shuttles/tt04/full_gds.png)
+
+### Tiny Tapeout 03p5
+
+[![Tiny Tapeout 03p5](shuttles/tt03p5/full_gds.png)](shuttles/tt03p5/full_gds.png)
+
+### Tiny Tapeout 3
+
+[![Tiny Tapeout 3](shuttles/tt03/full_gds.png)](shuttles/tt03/full_gds.png)
+
+### Tiny Tapeout 2
+
+[![Tiny Tapeout 2](shuttles/tt02/full_gds.png)](shuttles/tt02/full_gds.png)
+
+## Logic Density Renders
+
+* [Tiny Tapeout 6](shuttles/tt06/logic_density.png)
+* [Tiny Tapeout 5](shuttles/tt05/logic_density.png)
+* [Tiny Tapeout 4](shuttles/tt04/logic_density.png)
+* [Tiny Tapeout 03p5](shuttles/tt03p5/logic_density.png)
+* [Tiny Tapeout 3](shuttles/tt03/logic_density.png)
+* [Tiny Tapeout 2](shuttles/tt02/logic_density.png)
+
+
+## Regenerating the renders
+
+To regenerate the PNG files for a shuttle, run the following commands:
+
+```bash
+cd scripts
+pip install -r requirements.txt
+python render.py <shuttle>
+```
+
+Where `<shuttle>` is the identifier of the shuttle (e.g. tt04).
+
+You can also specify the scale of the render by passing the `--scale` argument. For example, to render the full GDS of shuttle tt04 at 2x scale:
+
+```bash
+python render.py tt04 --scale 2
+```
+
+## License
+
+The chip renders are licensed under the [CC BY 4.0](https://creativecommons.org/licenses/by/4.0/) license.
---
+++ scripts/render.py
@@ -0,0 +1,133 @@
+# SPDX-License-Identifier: Apache-2.0
+# This script creates a full-die render of a given Tiny Tapeout shuttle.
+# Copyright (C) 2024 Tiny Tapeout LTD
+# Author: Uri Shaked
+
+import argparse
+import json
+import logging
+import sys
+import urllib.parse
+import urllib.request
+from pathlib import Path
+from typing import List, Optional
+
+from klayout.lay import LayoutView
+
+# In legacy shuttles, we don't have an OAS file of the complete chip, rather a GDS file with just Tiny Tapeout tiles.
+LEGACY_SHUTTLES = ["tt02", "tt03", "tt03p5"]
+SCRIPT_DIR = Path(__file__).parent
+
+
+def download_gds(shuttle_id: str) -> Path:
+    extension = "oas" if shuttle_id not in LEGACY_SHUTTLES else "gds"
+    target_path = SCRIPT_DIR / "gds" / f"{shuttle_id}.{extension}"
+    if target_path.exists():
+        logging.info(f"Found existing GDS file at {target_path}, skipping download")
+        return target_path
+
+    # Download the main index file from the Tiny Tapeout server
+    url = "https://index.tinytapeout.com/index.json"
+    logging.info(f"Downloading index file from {url}")
+    response = urllib.request.urlopen(
+        urllib.request.Request(url, headers={"User-Agent": "Mozilla/5.0"})
+    )
+    index = json.load(response)
+    shuttles = index.get("shuttles", {})
+    shuttle = next(
+        (shuttle for shuttle in shuttles if shuttle["id"] == shuttle_id), None
+    )
+    if not shuttle:
+        logging.error(f"Shuttle {shuttle_id} not found in the index")
+        sys.exit(1)
+
+    gds_url = shuttle["gds_url"]
+    logging.info(f"Downloading GDS file from {gds_url}")
+
+    response = urllib.request.urlopen(gds_url)
+    with open(target_path, "wb") as f:
+        if gds_url.endswith(".gz"):
+            import gzip
+
+            with gzip.GzipFile(fileobj=response) as gz:
+                f.write(gz.read())
+        else:
+            f.write(response.read())
+
+    return target_path
+
+
+def render_gds(
+    gds_path: str,
+    output_path: str,
+    scale: float = 1.0,
+    only_layers: Optional[List[str]] = None,
+    hide_layers: Optional[List[str]] = None,
+):
+    BOUNDARY_LAYER = "prBoundary.boundary"
+
+    lv = LayoutView()
+    lv.load_layout(gds_path)
+    lv.max_hier()
+    lv.load_layer_props(SCRIPT_DIR / "lyp/sky130A.lyp")
+    lv.set_config("background-color", "#ffffff")
+    lv.set_config("grid-visible", "false")
+    lv.set_config("text-visible", "false")
+    lv.zoom_fit()
+
+    bbox = None
+    for layer in lv.each_layer():
+        layer_name = layer.name.split("-")[0].strip() if "-" in layer.name else ""
+        if layer_name == BOUNDARY_LAYER:
+            bbox = layer.bbox()
+            layer.visible = True
+        elif only_layers is not None:
+            layer.visible = layer_name in only_layers
+        elif hide_layers is not None:
+            layer.visible = layer_name not in hide_layers and layer_name != ""
+        else:
+            layer.visible = layer_name != ""  # Hides the fill layers
+
+    if bbox is None:
+        raise ValueError(f"No bounding box found for '{BOUNDARY_LAYER}' layer")
+    lv.zoom_box(bbox)
+
+    lv.save_image(output_path, int(bbox.width() * scale), int(bbox.height() * scale))
+    lv.destroy()
+
+
+def main(shuttle_id: str, scale: float = 1.0):
+    gds_file = download_gds(shuttle_id)
+    png_dir = SCRIPT_DIR / ".." / "shuttles" / shuttle_id
+    png_dir.mkdir(parents=True, exist_ok=True)
+
+    logging.info(f"Rendering {png_dir / 'full_gds.png'}")
+    render_gds(
+        gds_file,
+        png_dir / "full_gds.png",
+        scale=scale,
+        hide_layers=["areaid.standardc", "areaid.lowTapDensity"],
+    )
+
+    logging.info(f"Rendering {png_dir / 'logic_density.png'}")
+    render_gds(
+        gds_file,
+        png_dir / "logic_density.png",
+        scale=scale,
+        only_layers=["li1.drawing", "li1.pin"],
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Update shuttle index")
+    parser.add_argument("shuttle_id", type=str, help="Shuttle ID")
+    parser.add_argument(
+        "--scale",
+        type=float,
+        default=1.0,
+        help="Scale factor for the output image",
+    )
+
+    args = parser.parse_args()
+    logging.basicConfig(level=logging.INFO)
+    main(args.shuttle_id, scale=args.scale)
